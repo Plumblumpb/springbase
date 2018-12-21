bfe405447de4b1940736622271e737e4040e646a
Plumblumpb <2573350997@qq.com>
2018-12-21 16:38:22 +0800

    1.修改springdemo 测试。

--- springdemo/src/main/java/Main.java
+++ springdemo/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.MessageService;
@@ -12,11 +13,11 @@ public class Main {
     public static void main(String[] args) {
         // 用我们的配置文件来启动一个 ApplicationContext
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application.xml");
-//        ApplicationContext context1 = new FileSystemXmlApplicationContext("classpath:application.xml");
+//        ApplicationContext context1 = new FileSystemXmlApplicationContext("D:application.xml");
 //        ApplicationContext context2 = new AnnotationConfigApplicationContext("classpath:application.xml");
 
         System.out.println("context 启动成功");
-
+        BeanFactory beanFactory = context;
         // 从 context 中取出我们的 Bean，而不是用 new service.MessageServiceImpl() 这种方式
         MessageService messageService = context.getBean(MessageService.class);
         // 这句将输出: hello world
