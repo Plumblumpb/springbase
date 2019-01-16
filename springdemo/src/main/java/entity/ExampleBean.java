63e87b136bd7a1981b67d471f13a23f6530390f8
Plumblumpb <2573350997@qq.com>
2019-01-16 09:56:34 +0800

    1.修改springdemo 测试。

--- springdemo/src/main/java/entity/ExampleBean.java
+++ springdemo/src/main/java/entity/ExampleBean.java
@@ -1,9 +1,12 @@
 package entity;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @Auther: cpb
  * @Date: 2018/12/26 10:32
  * @Description:
  */
+@Component
 public class ExampleBean {
 }
